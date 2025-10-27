6bc0c58cf1a0a5db83d2c08c31b741aee9c15dcc
Victoria Sun <victoriasun1230@gmail.com>
2025-10-27 10:49:36 -0700

    notes lol

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,1 +1,2 @@
+# INTEGRATION STEP
 # This is intended to be an event handler that helps communicate between UI and backend
---
+++ backend/src/rgb_to_lab.py
@@ -0,0 +1,3 @@
+# full process of converting RGB gamma (rawest form of color read from picture)
+# to corrected RGB to XYZ to LAB
+# will require import of other files to use helper functions
